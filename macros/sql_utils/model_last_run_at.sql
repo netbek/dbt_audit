a6040e1d081becbfa4a0d8e3ba5c633f2b22d03a
Hein Bekker <hein@netbek.co.za>
2024-10-25 06:53:47 +0200

    Use adapter method

--- macros/sql_utils/model_last_run_at.sql
+++ macros/sql_utils/model_last_run_at.sql
@@ -1,6 +1,6 @@
 {% macro model_last_run_at(model, status=none) %}
     select max(run_start_at)
-    from {{ api.Relation.create(schema=var('dbt_audit_schema'), identifier='dbt_run') }}
+    from {{ adapter.get_relation(database=this.database, schema=var('dbt_audit_schema', this.schema), identifier='dbt_run') }}
     where
         node_id = '{{ model.unique_id }}'
         {% if status %}
